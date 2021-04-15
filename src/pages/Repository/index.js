cd08863ff7ad64e645730a78cc13903474b8c4ca
guijfalves <gui.jfalves@gmail.com>
2021-04-15 07:44:12 -0300

    Add global styles

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Repository() {
-    return <h1>Repository</h1>
+    return <h1>Repository</h1>;
 }
 
 export default Repository;
--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Repository from './pages/Repository';
 
-function Routes(){
-    return(
+function Routes() {
+    return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Main} />
                 <Route path="/repository" component={Repository} />
             </Switch>
         </BrowserRouter>
-    )
+    );
 }
 
 export default Routes;
